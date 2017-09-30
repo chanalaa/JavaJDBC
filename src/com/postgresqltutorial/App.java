3a70407ae62b447c6f1e0f48ded4ecea838e2458
Anita Chan <anita.chan@live.com>
2017-09-30 21:26:42 +0100

    Added first value from text file into new histogram table.

--- src/com/postgresqltutorial/App.java
+++ src/com/postgresqltutorial/App.java
@@ -25,31 +25,32 @@ public class App{
     public static void main(String[] args) throws SQLException, FileNotFoundException {
         String filepath = "/Users/Chan/NetBeansProjects/PostgreSQLJDBC/src/com/postgresqltutorial/grades.txt";
         Scanner scan = new Scanner(new File(filepath));
-        
+        ResultSet result;
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url, user, password);
             System.out.println("Connected to the PostgreSQL server successfully.");
             
-            String sql = "update histogram set number=?";
+            String sql = "INSERT into histogram (id, number) VALUES (?,?)";
             PreparedStatement preparedStatement = conn.prepareStatement(sql);
-            
+            int index = 1;
             while(scan.hasNext()) {
-            String num = scan.nextLine();
-            preparedStatement.setString(1,num);
+                String num = scan.nextLine();
+                
+                preparedStatement.setString(index,num);
+                index++;
+            }
+        
+        result = preparedStatement.executeQuery();
+        while (result.next()) {
+            String number = result.getString("number"); 
+            System.out.println(number);
         }
             
-            ResultSet result = preparedStatement.executeQuery();
-        
-        while (result.next()) {
-            String names = result.getString("number"); 
-            System.out.println(names);
-        }
-        
             } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-    
+        
     }
 }
 
