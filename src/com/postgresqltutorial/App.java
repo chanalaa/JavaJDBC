2ae1cc8b9428a534b6b3c48f4dbc69eaf4f328c3
Anita Chan <anita.chan@live.com>
2017-09-30 19:49:21 +0100

    Added scanner to scan in text file

--- src/com/postgresqltutorial/App.java
+++ src/com/postgresqltutorial/App.java
@@ -5,12 +5,15 @@
  */
 package com.postgresqltutorial;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 
 public class App{
@@ -19,21 +22,27 @@ public class App{
     private static String user = "Chan";
     private static String password = "";
  
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, FileNotFoundException {
+        String filepath = "/Users/Chan/NetBeansProjects/PostgreSQLJDBC/src/com/postgresqltutorial/grades.txt";
+        Scanner scan = new Scanner(new File(filepath));
         
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url, user, password);
             System.out.println("Connected to the PostgreSQL server successfully.");
-        
-        
-        Statement statement = conn.createStatement();
-        PreparedStatement preparedStatement = conn.prepareStatement("SELECT * FROM people");
-        ResultSet result = preparedStatement.executeQuery();
-        //ResultSet result = statement.executeQuery("SELECT * FROM people");
+            
+            String sql = "update histogram set number=?";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            
+            while(scan.hasNext()) {
+            String num = scan.nextLine();
+            preparedStatement.setString(1,num);
+        }
+            
+            ResultSet result = preparedStatement.executeQuery();
         
         while (result.next()) {
-            String names = result.getString("name"); 
+            String names = result.getString("number"); 
             System.out.println(names);
         }
         
