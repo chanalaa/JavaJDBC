372a898ead799304eb2703dbd6773be4ff8573f0
Anita Chan <anita.chan@live.com>
2017-10-01 14:13:11 +0100

    Added in delete statement

--- src/com/postgresqltutorial/App.java
+++ src/com/postgresqltutorial/App.java
@@ -15,45 +15,57 @@ import java.sql.Statement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
+public class App {
 
-public class App{
- 
     private static String url = "jdbc:postgresql://localhost:5432/Chan";
     private static String user = "Chan";
     private static String password = "";
- 
+
     public static void main(String[] args) throws SQLException, FileNotFoundException {
         String filepath = "/Users/Chan/NetBeansProjects/PostgreSQLJDBC/src/com/postgresqltutorial/grades.txt";
         Scanner scan = new Scanner(new File(filepath));
-            try {
-                connection();
-                useStatementMethodtoInsertIntoDatabase();
-            } catch (SQLException e) {
+        try {
+            connection();
+            useStatementMethodtoDeletefromDatabase();
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
 
     }
-    
+
     public static Connection connection() throws SQLException {
         Connection conn = null;
         conn = DriverManager.getConnection(url, user, password);
         return conn;
     }
-    
-    public static void createTableFromDatabase(){} // leave this one till last
-    public static void readFromDatabase(){    }
-    public static void useStatementMethodtoInsertIntoDatabase() throws SQLException{
-        Statement statement = connection().createStatement();
-        statement.executeUpdate("INSERT into histogram (id, number) values (101, 88)" );
-       
-    }
-    public static void useStatementMethodtoDeletefromDatabase(){}
-    public static void useStatementMethodtoUpdatefromDatabase(){}
-    public static void usePreparedStatementMethodtoInsertIntoDatabase(){
-    }
-    public static void usePreparedStatementMethodtoUpdateIntoDatabase(){}
-    public static void usePreparedStatementMethodtoDeleteIntoDatabase(){}
-    
-    
-}
 
+    public static void createTableFromDatabase() {
+    } // leave this one till last
+
+    public static void readFromDatabase() {
+    }
+
+    public static void useStatementMethodtoInsertIntoDatabase() throws SQLException {
+        Statement statement = connection().createStatement();
+        statement.executeUpdate("INSERT into histogram (id, number) values (101, 88)");
+    }
+
+    public static void useStatementMethodtoDeletefromDatabase() throws SQLException {
+        Statement statement = connection().createStatement();
+        statement.executeUpdate("DELETE from histogram where id = 101");
+
+    }
+
+    public static void useStatementMethodtoUpdatefromDatabase() {
+    }
+
+    public static void usePreparedStatementMethodtoInsertIntoDatabase() {
+    }
+
+    public static void usePreparedStatementMethodtoUpdateIntoDatabase() {
+    }
+
+    public static void usePreparedStatementMethodtoDeleteIntoDatabase() {
+    }
+
+}
